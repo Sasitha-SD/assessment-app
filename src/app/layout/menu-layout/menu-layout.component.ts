a270baaefcae4c3178c506db1bd5d61a26024b0f
Sasitha <sasitha@purple.lk>
2023-11-22 00:43:47 +0530

    [CHANGE] minor change in menu layout

--- src/app/layout/menu-layout/menu-layout.component.ts
+++ src/app/layout/menu-layout/menu-layout.component.ts
@@ -9,33 +9,35 @@ import {NgxUiLoaderService} from "ngx-ui-loader";
   styleUrls: ['./menu-layout.component.scss']
 })
 export class MenuLayoutComponent {
-  categories: any = {}
-  beefItems: any = {}
-  seafoodItems: any = {}
-  dessertItems: any = {}
+  categories: any = [];
+  beefItems: any = [];
+  seafoodItems: any = [];
+  dessertItems: any = [];
+
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private mealDbService: MealDbApiService, private ngxService: NgxUiLoaderService) {
   }
 
   async ngOnInit(): Promise<any> {
+    this.ngxService.start();
     this.categories = await this.mealDbService.getCategories();
     this.beefItems = await this.mealDbService.getItemByCategories('Beef');
     this.seafoodItems = await this.mealDbService.getItemByCategories('Seafood');
     this.dessertItems = await this.mealDbService.getItemByCategories('Dessert');
-
+    this.ngxService.stop();
   }
 
   //switch items
   switchItems(index: any): NgIterable<any> {
     switch (index) {
       case 0: {
-        return this.beefItems.slice(0, 3)
+        return this.beefItems.slice(0, 3);
       }
       case 1: {
-        return this.seafoodItems.slice(0, 3)
+        return this.seafoodItems.slice(0, 3);
       }
       default: {
-        return this.dessertItems.slice(0, 3)
+        return this.dessertItems.slice(0, 3);
       }
     }
   }
